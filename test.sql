431861258e9054c8bc0f70f4b07abe44bb4826a0
MAziz-0 <maastudentacc@gmail.com>
2021-06-25 05:39:57 +0000

    Pushing all files

---
+++ test.sql
@@ -0,0 +1,16 @@
+select count(*) from Track;
+select count(*) from Artist;mysql> source test.sql
++----------+
+| count(*) |
++----------+
+|     3503 |
++----------+
+1 row in set (0.00 sec)
+
++----------+
+| count(*) |
++----------+
+|      275 |
++----------+
+1 row in set (0.00 sec)
+
